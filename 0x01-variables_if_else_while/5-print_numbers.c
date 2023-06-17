bcb8044f5527011338b1d4ce5b096ab7b0f52ca5
EverestWhite <johnakujuaobi@gmail.com>
2023-06-16 19:48:04 -0700

    loops

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - This program prints base ten numbers from 0-9
+ *
+ *Return: Always 0 (Success)
+ */
+int main()
+{
+	int num;
+	for (num = 0; num <10; num++)
+	printf("%i", num);
+	putchar('\n');
+
+	return (0);
+}
