84b4b71d8171e5105ce3a0836b7943fa5653bb4d
EverestWhite <132360161+EverestWhite@users.noreply.github.com>
2023-06-17 03:53:20 +0100

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,10 +5,11 @@
  *
  *Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	int num;
-	for (num = 0; num <10; num++)
+	
+	for (num = 0; num < 10; num++)
 	printf("%i", num);
 	putchar('\n');
 
