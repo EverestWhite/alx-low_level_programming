a8ad366dcf33f1e511c5f7ef085b5e91554307ca
EverestWhite <johnakujuaobi@gmail.com>
2023-06-17 03:51:40 -0700

    loops

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
- * main- comparea random number with 5
- * Return: Alway 0 (Success)
- */
-
-=======
 * main - comparing a random number with 5
 * Return: Always 0 (Success)
 */
->>>>>>> 330f07a65288307b53059f43a36979b9a27406bf
 int main(void)
 {
 	char c = 'a';
