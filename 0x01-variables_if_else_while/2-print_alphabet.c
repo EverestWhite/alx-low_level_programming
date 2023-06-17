96ec07935fde9282f8c77b3b58b428fc4faf2d17
EverestWhite <132360161+EverestWhite@users.noreply.github.com>
2023-06-17 02:23:35 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+#include <stdlib.h>
+
+/**
+*This program prints the alphabets in lowercase
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+
+Int main()
+{
+     Char ch;
+         for (ch='a'; ch<='z'; ch++)
+           {
+             putchar("%c\n", ch\n);
+            }
+return (0);
+}
