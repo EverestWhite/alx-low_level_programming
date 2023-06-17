174bb795b0640338f0e2db6c58f6368784bf7307
EverestWhite <johnakujuaobi@gmail.com>
2023-06-16 20:30:37 -0700

    loops

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints lowercase alphabets in reverse
+* 
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+	putchar(alpha);
+/* measures the values of the
+ * characters from highest to lowest and prints
+ */
+	putchar('\n');
+
+	return (0);
+}
