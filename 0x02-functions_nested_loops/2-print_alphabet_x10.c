82cd54d6f65e0c49e4ce972f3ef90032c4c00ffa
EverestWhite <johnakujuaobi@gmail.com>
2023-06-20 16:10:10 -0700

    C functions and loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <sdtio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints lower case alphabets 20x
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, z;
+
+	for (i = 0; i <= 9; i++)
+		{
+			for (z = 97; z <= 122; z++)
+			{
+				_putchar(z);
+			}
+		_putchar('\n');
+		}
+	return (0);
+}
