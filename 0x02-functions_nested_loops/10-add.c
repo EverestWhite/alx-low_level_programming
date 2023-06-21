9e459fb3c700211a171a387ecd05257a9a587d43
EverestWhite <johnakujuaobi@gmail.com>
2023-06-20 17:22:08 -0700

    C functions and loop

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds numbers 'x' and 'y'
+ *
+ * @x: first number.
+ * @y: second number.
+ *
+ * Return: sum of the two numbers.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
