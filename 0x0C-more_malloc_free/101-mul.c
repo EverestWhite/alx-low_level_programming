cebbe4db7f43cfd27a7ba7e0f6c5693a05c7bfc9
EverestWhite <132360161+EverestWhite@users.noreply.github.com>
2023-07-14 22:16:27 +0100

    more examples on memory allocation

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,12 +1,14 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * adding_all_mul - sum all the addition to know the multiplication result
+ * adding_all_mul - fxn that sums all the addition to know
+ * the multiplication result
  * @a: number 1
  * @len_a: lenght of number 1
  * @b: number 2
  * @len_b: lenght of number 2
- * Return: Addition pointer to the total resul of the  multiplication
+ * Return: Additn pointer to the total result of the multiplcn
  */
 add_t *adding_all_mul(char *a, int len_a, char *b, int len_b)
 {
@@ -14,9 +16,7 @@ add_t *adding_all_mul(char *a, int len_a, char *b, int len_b)
 	int i = 0, j = 0, carry = 0;
 
 	result = malloc(sizeof(add_t));
-
 	result->next = NULL, result->n_dig = 0, result->len_r = len_a + len_b;
-
 	result->n_add = malloc(sizeof(char) * result->len_r);
 
 	for (i = 0; i < result->len_r; i++)
@@ -25,16 +25,17 @@ add_t *adding_all_mul(char *a, int len_a, char *b, int len_b)
 	for (i = len_a - 1; i >= 0; i--)
 	{
 		carry = 0;
-		for (j = len_b - 1; j >= 0; j--)
+	for (j = len_b - 1; j >= 0; j--)
+		{
+		carry += (a[i] - '0') * (b[j] - '0');
+		carry += result->n_add[i + j + 1] - '0';
+		}
+		
+		result->n_add[i + j + 1] = (carry % 10) + '0';
+		carry /= 10;
 	}
-	carry += (a[i] - '0') * (b[j] - '0');
-	carry += result->n_add[i + j + 1] - '0';
-	result->n_add[i + j + 1] = (carry % 10) + '0';
-	carry /= 10;
-}
 if (carry)
 	result->n_add[i + j + 1] = (carry % 10) + '0';
-	}
 if (result->n_add[0] != '0')
 	result->n_dig = len_a + len_b;
 else
